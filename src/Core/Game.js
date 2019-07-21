1aa3f01b3e99e944eea58038241764990a1d4145
Vitalii Kalaida <nevadskiy@gmail.com>
2019-07-22 00:43:31 +0300

    Add tilemap

--- src/Core/Game.js
+++ src/Core/Game.js
@@ -1,13 +1,11 @@
 /* eslint-disable import/extensions */
-import Vector from '../Utils/Vector.js';
 
 export default class Game {
-  constructor(controller, view) {
+  constructor(view, tileMap) {
     // TODO: add World object
     // TODO: add layers to allow draw entities at the correct layer
-    // Probably remove controller from here and left only with player
-    this.controller = controller;
     this.view = view;
+    this.tileMap = tileMap;
     this.entities = [];
     this.friction = 0.9;
     this.gravity = 0.5;
@@ -20,23 +18,19 @@ export default class Game {
   }
 
   render() {
+    // TODO: remove it after all world render will be called
     this.view.clear();
+
+    this.tileMap.forEach((tile) => {
+      tile.render(this.view);
+    });
 
     this.entities.forEach((entity) => {
       entity.render(this.view);
     });
-
-    this.renderControllerStatus();
   }
 
   add(entity) {
     this.entities.push(entity);
-  }
-
-  renderControllerStatus() {
-    this.view.text(`UP: ${this.controller.state.up ? 'PRESSED' : 'NO'}`, new Vector(10, 20));
-    this.view.text(`LEFT: ${this.controller.state.left ? 'PRESSED' : 'NO'}`, new Vector(10, 40));
-    this.view.text(`RIGHT: ${this.controller.state.right ? 'PRESSED' : 'NO'}`, new Vector(10, 60));
-    this.view.text(`DOWN: ${this.controller.state.down ? 'PRESSED' : 'NO'}`, new Vector(10, 80));
   }
 }
