1dfa7f8e80cfe0318663942bc7470bc512d85f6a
Vitalii Kalaida <nevadskiy@gmail.com>
2019-07-22 00:44:21 +0300

    Refactor game loading with promises

--- index.js
+++ index.js
@@ -6,43 +6,61 @@ import View from './src/Graphic/View.js';
 import CanvasFactory from './src/Graphic/CanvasFactory.js';
 import Fps from './src/Fps.js';
 import Player from './src/Player.js';
-import Ground from './src/Entities/Ground.js';
-import Vector from './src/Utils/Vector.js';
 import Size from './src/Utils/Size.js';
 import Position from './src/Utils/Position.js';
-import Sprite from './src/Graphic/Sprite.js';
+import SpriteMap from './src/Graphic/SpriteMap.js';
+import TileMap from './src/Game/TileMap.js';
+import ImageLoader from './src/Loaders/ImageLoader.js';
+import TileMapLoader from './src/Loaders/TileMapLoader.js';
 
-// View init
-const view = new View(CanvasFactory.create(new Size(640, 480), document.body));
+// TODO: left all hard-coded logic here and extract independent logic into classes
 
-// TODO: resolve with promise loader (add Promise.all([spriteLoader, levelBuilder]).then(game.start()))
-const worldSprite = new Sprite('src/Resources/world-sprite.png');
-worldSprite.define('ground', new Position(0, 0), new Size(16, 16));
-worldSprite.define('bricks', new Position(16, 0), new Size(16, 16));
+window.addEventListener('load', () => {
+  Promise.all([
+    ImageLoader.load('src/Resources/world-sprite.png')
+      .then((image) => {
+        const sprite = new SpriteMap(image);
 
-const charactersSprite = new Sprite('src/Resources/characters-sprite.gif');
-charactersSprite.define('little-mario', new Position(276, 44), new Size(16, 16));
+        sprite.define('sky', new Position(48, 361), new Size(16, 16));
+        sprite.define('ground', new Position(0, 0), new Size(16, 16));
+        sprite.define('bricks', new Position(16, 0), new Size(16, 16));
 
-// Controller
-const controller = new Controller();
+        return sprite;
+      })
+      .then((sprite) => {
+        const tileMap = new TileMap(new Size(16, 16));
 
-// Game init
-const game = new Game(controller, view);
-const engine = new Engine(game);
+        const mapping = {
+          '.': 'sky',
+          '#': 'ground',
+          '%': 'bricks',
+        };
 
-// Game scene
-// TODO: ground building (extract into level builder)
+        return TileMapLoader.fromTxt('src/Resources/1-1.txt', tileMap, mapping, sprite);
+      }),
 
-for (let i = 0; i < 10; i += 1) {
-  game.add(new Ground(new Vector(i * 16, 200 + 20), new Size(16, 16), worldSprite.get('ground')));
-  game.add(new Ground(new Vector(i * 16, 200 + 20 + 16), new Size(16, 16), worldSprite.get('bricks')));
-}
+    ImageLoader.load('src/Resources/characters-sprite.gif')
+      .then((image) => {
+        const sprite = new SpriteMap(image);
 
-game.add(new Fps());
-game.add(new Player(controller, game.friction, game.gravity, charactersSprite.get('little-mario')));
+        sprite.define('little-mario', new Position(276, 44), new Size(16, 16));
 
-// Events registering
-// TODO: Probably extract this event into their own classes
-window.addEventListener('keydown', controller.handleKeyDown.bind(controller));
-window.addEventListener('keyup', controller.handleKeyUp.bind(controller));
-window.addEventListener('load', engine.run.bind(engine));
+        return new Player(new Controller(), sprite.get('little-mario'));
+      }),
+  ]).then(([tileMap, player]) => {
+    console.log(tileMap);
+
+    // View init
+    const view = new View(CanvasFactory.create(new Size(640, 480), document.body));
+
+    // Game init
+    const game = new Game(view, tileMap);
+    const engine = new Engine(game);
+
+    // Add base entities
+    game.add(player);
+    game.add(new Fps());
+
+    engine.run();
+  });
+});
