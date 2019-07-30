024ee740793ca2674b21635387866f4530e02a2f
Vitalii Kalaida <nevadskiy@gmail.com>
2019-07-31 01:01:56 +0300

    Merge position and vector into one single object

--- src/Utils/Vector.js
+++ src/Utils/Vector.js
@@ -1,12 +1,37 @@
-/* eslint-disable import/extensions */
-import Position from './Position.js';
+export default class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-export default class Vector extends Position {
+  static zero() {
+    return new Vector(0, 0);
+  }
+
+  setX(x) {
+    this.x = x;
+  }
+
+  setY(y) {
+    this.y = y;
+  }
+
   plus(vector) {
     return new Vector(
       vector.x === null ? this.x : this.x + vector.x,
       vector.y === null ? this.y : this.y + vector.y,
     );
+  }
+
+  minus(vector) {
+    return new Vector(
+      vector.x === null ? this.x : this.x - vector.x,
+      vector.y === null ? this.y : this.y - vector.y,
+    );
+  }
+
+  minusX(vector) {
+    return new Vector(this.x - vector.x, this.y);
   }
 
   multiply(vector) {
@@ -20,6 +45,20 @@ export default class Vector extends Position {
     return new Vector(
       this.x * times,
       this.y * times,
+    );
+  }
+
+  round() {
+    return new Vector(
+      Math.floor(this.x),
+      Math.floor(this.y),
+    );
+  }
+
+  ceil() {
+    return new Vector(
+      Math.ceil(this.x),
+      Math.ceil(this.y),
     );
   }
 }
