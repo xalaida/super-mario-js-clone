c468ea98978cbe6cd4efff6d55eebcd5813fbafa
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-03 19:14:09 +0300

    Move View into Graphic directory

--- src/Engine/Graphic/View.js
+++ src/Engine/Graphic/View.js
@@ -1,20 +1,43 @@
 export default class View {
+  /**
+   * Create a view for game rendering control
+   *
+   * @param context
+   */
   constructor(context) {
     this.context = context;
   }
 
+  /**
+   * Clear view context
+   */
   clear() {
     this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
   }
 
-  rectangle(position, size, color = '#1A202C') {
+  /**
+   * Draw a rectangle
+   *
+   * @param position
+   * @param size
+   * @param color
+   */
+  rectangle(position, size, color = 'blue') {
     this.context.save();
     this.context.fillStyle = color;
     this.context.fillRect(Math.floor(position.x), Math.floor(position.y), size.width, size.height);
     this.context.restore();
   }
 
-  text(text, vector, color = '#1A202C', size = '14px') {
+  /**
+   * Draw a text
+   *
+   * @param text
+   * @param vector
+   * @param color
+   * @param size
+   */
+  text(text, vector, color = 'black', size = '14px') {
     this.context.save();
     this.context.font = size;
     this.context.fillStyle = color;
@@ -22,11 +45,18 @@ export default class View {
     this.context.restore();
   }
 
+  /**
+   * Draw a box outline
+   *
+   * @param position
+   * @param size
+   * @param color
+   */
   outline(position, size, color = 'green') {
     this.context.save();
     this.context.strokeStyle = color;
     this.context.beginPath();
-    this.context.rect(position.x, position.y, size.width, size.height);
+    this.context.rect(Math.floor(position.x), Math.floor(position.y), size.width, size.height);
     this.context.stroke();
     this.context.restore();
   }
@@ -38,11 +68,13 @@ export default class View {
    * @param position
    * @param size
    */
-  image(image, position, size) {
+  spriteImage(image, position, size) {
     this.context.drawImage(
       image.source,
-      image.position.x, image.position.y, image.size.width, image.size.height,
-      Math.floor(position.x), Math.floor(position.y), size.width, size.height,
+      image.position.x, image.position.y,
+      image.size.width, image.size.height,
+      Math.floor(position.x), Math.floor(position.y),
+      size.width, size.height,
     );
   }
 }
