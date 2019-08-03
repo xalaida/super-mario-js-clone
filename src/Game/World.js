939f8d738be7cfba4eb2f35516bbc2aa7ea6a576
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-03 19:04:42 +0300

    Remove old classes

--- src/Game/World.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/extensions */
-import Vector from '../Utils/Vector.js';
-
-export default class World {
-  constructor() {
-    this.friction = new Vector(100, 100);
-    this.gravity = new Vector(0, 800);
-  }
-}
--- src/Graphic/Image.js
+++
@@ -1,7 +0,0 @@
-export default class Image {
-  constructor(source, position, size) {
-    this.source = source;
-    this.position = position;
-    this.size = size;
-  }
-}
