4ade6b0a228404a615ced584b0ed8d02a879d2f6
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-03 19:12:12 +0300

    Refactor controller with keybinds

--- src/Engine/Input/Controller.js
+++ src/Engine/Input/Controller.js
@@ -1,6 +1,8 @@
+import Vector from '../Math/Vector.js';
+
 export default class Controller {
-  constructor(controls) {
-    this.controls = Object.assign(Controller.defaultControls(), controls);
+  constructor(keyBinds) {
+    this.keyBinds = Object.assign(Controller.defaultControls(), keyBinds);
     this.keys = new Map();
     this.state = new Map();
     this.init();
@@ -17,12 +19,12 @@ export default class Controller {
   }
 
   init() {
-    Object.keys(this.controls).forEach((key) => {
-      this.keys.set(key, this.controls[key]);
+    Object.keys(this.keyBinds).forEach((key) => {
+      this.keys.set(key, this.keyBinds[key]);
     });
 
-    Object.keys(this.controls).forEach((key) => {
-      this.state.set(this.controls[key], false);
+    Object.keys(this.keyBinds).forEach((key) => {
+      this.state.set(this.keyBinds[key], false);
     });
   }
 
@@ -52,5 +54,12 @@ export default class Controller {
 
   isPressed(id) {
     return this.state.get(id);
+  }
+
+  render(view) {
+    Object.values(this.keyBinds).forEach((action, index) => {
+      const debugString = `${action.toUpperCase()}: ${this.isPressed(action) ? 'Pressed' : 'Released'}`;
+      view.text(debugString, new Vector(10, 20 + (index * 20)));
+    });
   }
 }
