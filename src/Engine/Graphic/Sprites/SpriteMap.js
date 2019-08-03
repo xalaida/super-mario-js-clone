6fa6bcbb0ab3607e1efb3d24beed496e52a55f0d
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-03 19:11:54 +0300

    Rename GameImage into SpriteImage

--- src/Engine/Graphic/Sprites/SpriteMap.js
+++ src/Engine/Graphic/Sprites/SpriteMap.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-param-reassign,import/extensions */
-import GameImage from './Image.js';
+import SpriteImage from './SpriteImage.js';
 
 export default class SpriteMap {
   constructor(source) {
@@ -8,7 +7,7 @@ export default class SpriteMap {
   }
 
   define(name, position, size) {
-    this.definitions.set(name, new GameImage(this.source, position, size));
+    this.definitions.set(name, new SpriteImage(this.source, position, size));
   }
 
   get(name) {
