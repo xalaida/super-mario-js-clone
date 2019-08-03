92f0eb2b2b61ee375c6e850250746fb2d2e94661
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-03 19:06:30 +0300

    Add FPS class for frameRate debugging

---
+++ src/Engine/Debug/Fps.js
@@ -0,0 +1,24 @@
+import Vector from '../Math/Vector.js';
+
+export default class Fps {
+  constructor(config) {
+    this.config = config;
+    this.value = 0;
+    this.timestamp = 0;
+    this.previousTimestamp = 0;
+  }
+
+  update() {
+    this.timestamp = performance.now();
+
+    const dt = this.timestamp - this.previousTimestamp;
+
+    this.previousTimestamp = this.timestamp;
+
+    this.value = Math.round(1 / (dt / 1000));
+  }
+
+  render(view) {
+    view.text(`FPS: ${this.value}`, new Vector(this.config.width - 50, 20));
+  }
+}
