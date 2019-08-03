f511f0224bffe7a7060d3d2085ee324d5dba5558
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-03 19:07:48 +0300

    Finally feature immutable vector object

--- src/Engine/Math/Vector.js
+++ src/Engine/Math/Vector.js
@@ -4,46 +4,8 @@ export default class Vector {
     this.y = y;
   }
 
-  copy() {
-    return new Vector(this.x, this.y);
-  }
-
   static zero() {
     return new Vector(0, 0);
-  }
-
-  setX(x) {
-    this.x = x;
-    return this;
-  }
-
-  setY(y) {
-    this.y = y;
-    return this;
-  }
-
-  plus(vector) {
-    this.x = vector.x === null ? this.x : this.x + vector.x;
-    this.y = vector.y === null ? this.y : this.y + vector.y;
-    return this;
-  }
-
-  minus(vector) {
-    this.x = vector.x === null ? this.x : this.x - vector.x;
-    this.y = vector.y === null ? this.y : this.y - vector.y;
-    return this;
-  }
-
-  multiply(vector) {
-    this.x = vector.x === null ? this.x : this.x * vector.x;
-    this.y = vector.y === null ? this.y : this.y * vector.y;
-    return this;
-  }
-
-  scale(times) {
-    this.x *= times;
-    this.y *= times;
-    return this;
   }
 
   floor() {
@@ -56,5 +18,83 @@ export default class Vector {
     this.x = Math.ceil(this.x);
     this.y = Math.ceil(this.y);
     return this;
+  }
+
+  set(vector) {
+    this.x = vector.x;
+    this.y = vector.y;
+    return this;
+  }
+
+  setX(x) {
+    this.x = x;
+    return this;
+  }
+
+  setY(y) {
+    this.y = y;
+    return this;
+  }
+
+  /**
+   *
+   * Below comes only with immutable methods
+   *
+   */
+
+  plus(vector) {
+    return new Vector(
+      vector.x === null ? this.x : this.x + vector.x,
+      vector.y === null ? this.y : this.y + vector.y,
+    );
+  }
+
+  plusX(x) {
+    return new Vector(
+      this.x + x,
+      this.y,
+    );
+  }
+
+  plusY(y) {
+    return new Vector(
+      this.x,
+      this.y + y,
+    );
+  }
+
+  minus(vector) {
+    return new Vector(
+      vector.x === null ? this.x : this.x - vector.x,
+      vector.y === null ? this.y : this.y - vector.y,
+    );
+  }
+
+  minusX(x) {
+    return new Vector(
+      this.x - x,
+      this.y,
+    );
+  }
+
+  minusY(y) {
+    return new Vector(
+      this.x,
+      this.y - y,
+    );
+  }
+
+  multiply(vector) {
+    return new Vector(
+      vector.x === null ? this.x : this.x * vector.x,
+      vector.y === null ? this.y : this.y * vector.y,
+    );
+  }
+
+  scale(times) {
+    return new Vector(
+      this.x * times,
+      this.y * times,
+    );
   }
 }
