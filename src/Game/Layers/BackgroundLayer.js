19ca00b5768b7102413c01be55afaeb312f0a1f2
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-08 23:50:43 +0300

    Feature background layer which fill the context with the given color

--- src/Game/Layers/BackgroundLayer.js
+++ src/Game/Layers/BackgroundLayer.js
@@ -1,9 +1,20 @@
 export default class BackgroundLayer {
-  constructor(backgroundTileMap) {
-    this.tileMap = backgroundTileMap;
+  /**
+   * BackgroundLayer constructor
+   *
+   * @param {TileMap} tileMap
+   */
+  constructor(tileMap) {
+    this.tileMap = tileMap;
   }
 
-  render(view) {
-    this.tileMap
+  /**
+   * Render the background layer
+   *
+   * @param {View} view
+   * @param {Camera} camera
+   */
+  render(view, camera) {
+    this.tileMap.render(view, camera);
   }
 }
