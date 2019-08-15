721d9be083d9e7f02a33de4c3065dc4f334bf675
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-15 23:03:46 +0300

    Add base Killable component

---
+++ src/Game/Components/Killable.js
@@ -0,0 +1,39 @@
+import Component from '../../Engine/Behaviour/Component.js';
+
+export default class Killable extends Component {
+  constructor(entity) {
+    super('killable', entity);
+    this.dead = false;
+    this.dying = false;
+    this.dyingTime = 0;
+    this.deadDelay = 2;
+  }
+
+  kill() {
+    this.dying = true;
+  }
+
+  revive() {
+    this.dead = false;
+    this.dying = false;
+    this.dyingTime = 0;
+  }
+
+  /**
+   * Update the killable component
+   */
+  update(deltaTime) {
+    if (this.dying) {
+      this.dyingTime += deltaTime;
+    }
+
+    if (this.dyingTime >= this.deadDelay) {
+      this.dead = true;
+
+      // TODO: refactor this (probably use event bus)
+      if (!this.entity.hasComponent('respawn')) {
+        game.sceneManager.scene.entities.delete('goomba');
+      }
+    }
+  }
+}
