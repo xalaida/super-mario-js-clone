be88f4c70af144d540abdabb7cddffb59dcb5a7f
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-15 20:51:53 +0300

    Add base Stomp component

---
+++ src/Game/Entities/Mario/Components/Stomp.js
@@ -0,0 +1,12 @@
+import Component from '../../../../Engine/Behaviour/Component.js';
+import Vector from '../../../../Engine/Math/Vector.js';
+
+export default class Stomp extends Component {
+  constructor(entity) {
+    super('stomp', entity);
+  }
+
+  update() {
+    // todo
+  }
+}
