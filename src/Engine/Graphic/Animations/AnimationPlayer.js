0cb540d6d2022c930b0dc186b1fd53860c669936
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-16 21:32:33 +0300

    Refactor AnimationPlayer

--- src/Engine/Graphic/Animations/AnimationPlayer.js
+++ src/Engine/Graphic/Animations/AnimationPlayer.js
@@ -1,20 +1,51 @@
-// TODO: rename to animation player (and main method play())
-// TODO: probably use play() as merge result of method switch + pull and add separate switch to allow switching without pulling
 export default class AnimationPlayer {
-  constructor(animationMap = null) {
-    this.animations = animationMap || new Map();
+  /**
+   * AnimationPlayer constructor
+   *
+   * @param {Map} animationsMap
+   */
+  constructor(animationsMap = null) {
+    this.animations = animationsMap || new Map();
     this.current = null;
   }
 
+  /**
+   * Add the animation to the player
+   *
+   * @param {String} name
+   * @param {Animation} animation
+   */
   add(name, animation) {
     this.animations.set(name, animation);
   }
 
+  /**
+   * Get the animation by the name
+   *
+   * @param {String} name
+   * @returns {Animation}
+   */
   get(name) {
     return this.animations.get(name);
   }
 
+  /**
+   * Continue playing the animation or switch to the new one if currently playing another animation
+   *
+   * @param {String} name
+   * @returns {SpriteImage}
+   */
   play(name) {
+    this.switch(name);
+    return this.pull();
+  }
+
+  /**
+   * Switch the current animation to the new one
+   *
+   * @param {String} name
+   */
+  switch(name) {
     this.guardUndefinedAnimation(name);
 
     if (this.current === name) {
@@ -24,16 +55,26 @@ export default class AnimationPlayer {
     this.current = name;
   }
 
+  /**
+   * Pull the currently playing animation frame
+   *
+   * @returns {SpriteImage}
+   */
   pull() {
     const animation = this.get(this.current);
 
-    const { frame } = animation;
+    const animationFrame = animation.frame;
 
     animation.update();
 
-    return frame;
+    return animationFrame;
   }
 
+  /**
+   * Guard against undefined animation
+   *
+   * @param {String} name
+   */
   guardUndefinedAnimation(name) {
     if (!this.animations.has(name)) {
       throw new Error(`Undefined animation ${name}`);
