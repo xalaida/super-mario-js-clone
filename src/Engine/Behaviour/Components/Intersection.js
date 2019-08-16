eed0c801c870ecb22c78f9c5145321ccb6b22304
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-16 21:07:25 +0300

    Add doc blocks

--- src/Engine/Behaviour/Components/Intersection.js
+++ src/Engine/Behaviour/Components/Intersection.js
@@ -1,19 +1,35 @@
 import Component from '../Component.js';
 
 export default class Intersection extends Component {
+  /**
+   * Intersection constructor
+   *
+   * @param {Entity} entity
+   */
   constructor(entity) {
     super('intersection', entity);
     this.entity = null;
   }
 
+  /**
+   * Set the intersected entity
+   *
+   * @param {Entity} entity
+   */
   intersects(entity) {
     this.entity = entity;
   }
 
+  /**
+   * Reset the intersected entity
+   */
   reset() {
     this.entity = null;
   }
 
+  /**
+   * Update the intersection component
+   */
   update() {
     this.reset();
   }
