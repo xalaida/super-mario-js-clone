432a1a78f93eeba19d1c2b6d22c1a799c95d32a1
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-16 20:41:13 +0300

    Add size setters and pixel factory method

--- src/Engine/Math/Size.js
+++ src/Engine/Math/Size.js
@@ -1,11 +1,53 @@
 import Vector from './Vector.js';
 
 export default class Size {
+  /**
+   * Size constructor
+   *
+   * @param {Number} width
+   * @param {Number} height
+   */
   constructor(width, height) {
     this.width = width;
     this.height = height;
   }
 
+  /**
+   * Pixel size factory method
+   *
+   * @returns {Size}
+   */
+  static pixel() {
+    return new Size(1, 1);
+  }
+
+  /**
+   * Set the size width
+   *
+   * @param {Number} width
+   * @returns {Size}
+   */
+  setWidth(width) {
+    this.width = width;
+    return this;
+  }
+
+  /**
+   * Set the size height
+   *
+   * @param {Number} height
+   * @returns {Size}
+   */
+  setHeight(height) {
+    this.height = height;
+    return this;
+  }
+
+  /**
+   * Convert the size into the 2D vector
+   *
+   * @returns {Vector}
+   */
   toVector() {
     return new Vector(
       this.width,
