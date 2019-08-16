d0466b595141c2721a98e50a0cfaf19c30b31926
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-16 22:38:58 +0300

    Add memory human format support

--- src/Engine/Debug/Memory.js
+++ src/Engine/Debug/Memory.js
@@ -1,12 +1,44 @@
 import Vector from '../Math/Vector.js';
 
-export default class Fps {
+export default class Memory {
+  /**
+   * Memory constructor
+   *
+   * @param {Config} config
+   */
   constructor(config) {
-    this.config = config;
+    this.positionLineUsed = new Vector(config.width - 120, 60);
+    this.positionLineTotal = new Vector(config.width - 120, 40);
   }
 
+  /**
+   * Render the memory info
+   *
+   * @param {View} view
+   */
   render(view) {
-    view.text(`Memory used: ${performance.memory.usedJSHeapSize}`, new Vector(this.config.width - 120, 60));
-    view.text(`Memory total: ${performance.memory.totalJSHeapSize}`, new Vector(this.config.width - 120, 40));
+    view.text(`Memory used: ${Memory.format(performance.memory.usedJSHeapSize)}`, this.positionLineUsed);
+    view.text(`Memory total: ${Memory.format(performance.memory.totalJSHeapSize)}`, this.positionLineTotal);
+  }
+
+  /**
+   * Translate bytes amount to human readable format
+   *
+   * @param {Number} bytes
+   * @param {Number} decimals
+   * @returns {string}
+   */
+  static format(bytes, decimals = 2) {
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return `${parseFloat((bytes / (k ** i)).toFixed(dm))} ${sizes[i]}`;
   }
 }
