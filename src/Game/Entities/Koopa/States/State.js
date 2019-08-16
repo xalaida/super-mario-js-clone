6524dd23032d1227b96c054a8c048ccc4fe5df05
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-16 23:59:38 +0300

    Feature the base Koopa's State class

---
+++ src/Game/Entities/Koopa/States/State.js
@@ -0,0 +1,42 @@
+/* eslint-disable class-methods-use-this */
+
+export default class State {
+  /**
+   * State constructor
+   *
+   * @param {Koopa} entity
+   */
+  constructor(entity) {
+    this.entity = entity;
+  }
+
+  /**
+   * Update the state
+   */
+  update() {
+    // State update function
+  }
+
+  /**
+   * On stomp handler
+   */
+  onStomp() {
+    throw new Error(`Define the onStomp() method in the ${this.constructor.name} class`);
+  }
+
+  /**
+   * On touch handler
+   */
+  onTouch() {
+    throw new Error(`Define the onTouch() method in the ${this.constructor.name} class`);
+  }
+
+  /**
+   * Get animation frame for the entity in the state
+   *
+   * @returns {SpriteImage}
+   */
+  getAnimationFrame() {
+    throw new Error(`Define the getAnimationFrame() method in the ${this.constructor.name} class`);
+  }
+}
