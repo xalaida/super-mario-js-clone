0eefd42e625f82a100abbbdbc822df0868cfc4db
Vitalii Kalaida <nevadskiy@gmail.com>
2019-08-17 23:12:53 +0300

    Move the Turbo component from shared components

---
+++ src/Game/Entities/Mario/Components/Turbo.js
@@ -0,0 +1,26 @@
+import Component from '../../../../Engine/Behaviour/Component.js';
+import Vector from '../../../../Engine/Math/Vector.js';
+
+export default class Turbo extends Component {
+  /**
+   * Turbo constructor
+   *
+   * @param {Entity} entity
+   */
+  constructor(entity) {
+    super('turbo', entity);
+  }
+
+  /**
+   * Update the turbo component
+   */
+  update() {
+    if (this.entity.controller.isPressed('actionA')) {
+      this.entity.component('movement').maxSpeed = game.config.player.turboMaxSpeed;
+      this.entity.component('movement').accelaration = new Vector(game.config.player.turboMaxSpeed, 0);
+    } else {
+      this.entity.component('movement').maxSpeed = game.config.player.maxSpeed;
+      this.entity.component('movement').accelaration = new Vector(game.config.player.acceleration, 0);
+    }
+  }
+}
